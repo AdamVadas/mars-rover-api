086a2ad7deb4d28b98545e86c56a48ef8efa8ffa
AdamVadas <adam.vadas.w17@gmail.com>
2020-11-21 19:54:18 +0100

    added checkbox

--- src/main/java/com/vadas/MarsRoverApi/dto/HomeDTO.java
+++ src/main/java/com/vadas/MarsRoverApi/dto/HomeDTO.java
@@ -13,6 +13,7 @@ public class HomeDTO {
     private Boolean cameraNavcam;
     private Boolean cameraPancam;
     private Boolean cameraMinites;
+    private Boolean rememberPreferences;
 
     public String getMarsApiRoverData() {
         return marsApiRoverData;
@@ -100,5 +101,13 @@ public class HomeDTO {
 
     public void setCameraMinites(Boolean cameraMinites) {
         this.cameraMinites = cameraMinites;
+    }
+
+    public Boolean getRememberPreferences() {
+        return rememberPreferences;
+    }
+
+    public void setRememberPreferences(Boolean rememberPreferences) {
+        this.rememberPreferences = rememberPreferences;
     }
 }
